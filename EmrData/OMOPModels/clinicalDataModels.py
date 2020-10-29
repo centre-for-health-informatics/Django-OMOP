892794a3b48502df7353e666f3049ebd145a9f3e
Oscar Chen <oscar.y.chen@gmail.com>
2020-10-29 14:42:42 -0600

    Added Standardized Derived Element Models

--- EmrData/OMOPModels/clinicalDataModels.py
+++ EmrData/OMOPModels/clinicalDataModels.py
@@ -6,16 +6,16 @@ from EmrData.OMOPModels.vocabularyModels import CONCEPT
 class PERSON(models.Model):
     person_id = models.BigIntegerField(primary_key=True)
     gender_concept_id = models.ForeignKey(CONCEPT, on_delete=models.DO_NOTHING,
-                                          limit_choices_to=Q(domain__domain_id='Gender') & Q(standard_concept='S'), related_name="+")
+                                          limit_choices_to=Q(domain__domain_id__icontains='Gender') & Q(standard_concept='S'), related_name="+")
     year_of_birth = models.IntegerField(null=True)
     month_of_birth = models.IntegerField(null=True)
     day_of_birth = models.IntegerField(null=True)
     birth_datetime = models.DateTimeField(null=True)
     death_datetime = models.DateField(null=True)
     race_concept_id = models.ForeignKey(CONCEPT, on_delete=models.DO_NOTHING,
-                                        limit_choices_to=Q(domain__domain_id='Race') & Q(standard_concept='S'), null=True, related_name="+")
+                                        limit_choices_to=Q(domain__domain_id__icontains='Race') & Q(standard_concept='S'), null=True, related_name="+")
     ethnicity_concept_id = models.ForeignKey(CONCEPT, on_delete=models.DO_NOTHING,
-                                             limit_choices_to=Q(domain__domain_id='Ethnicity') & Q(standard_concept='S'), null=True, related_name="+")
+                                             limit_choices_to=Q(domain__domain_id__icontains='Ethnicity') & Q(standard_concept='S'), null=True, related_name="+")
     # location_id TODO
     # provider_id TODO
     # care_site_id TODO
---
+++ EmrData/OMOPModels/standardizedDerivedModels.py
@@ -0,0 +1,47 @@
+from django.db.models.fields import AutoField, BigAutoField
+from EmrData.OMOPModels.vocabularyModels import CONCEPT
+from django.db import models
+from django.db.models import Q
+from EmrData.OMOPModels.clinicalDataModels import PERSON
+
+
+class DRUG_ERA(models.Model):
+    drug_era_id = models.BigAutoField()
+    person_id = models.ForeignKey(PERSON, on_delete=models.DO_NOTHING, related_name="drug_era")
+    drug_concept_id = models.ForeignKey(CONCEPT, on_delete=models.DO_NOTHING,
+                                        limit_choices_to=Q(domain__domain_id__icontains='Drug'), related_name="+")
+    drug_era_start_date = models.DateTimeField()
+    drug_era_end_date = models.DateTimeField()
+    drug_exposure_count = models.IntegerField(null=True)
+    gap_days = models.IntegerField(null=True)
+
+    class Meta:
+        db_table = "DRUG_ERA"
+
+
+class DOSE_ERA(models.Model):
+    dose_era_id = BigAutoField()
+    person_id = models.ForeignKey(PERSON, on_delete=models.DO_NOTHING, related_name="dose_era")
+    drug_concept_id = models.ForeignKey(CONCEPT, on_delete=models.DO_NOTHING,
+                                        limit_choices_to=Q(domain__domain_id__icontains='Drug'), related_name="+")
+    unit_concept_id = models.ForeignKey(CONCEPT, on_delete=models.DO_NOTHING,
+                                        limit_choices_to=Q(domain__domain_id__icontains='Unit'), related_name="+")
+    dose_value = models.FloatField()
+    dose_era_start_date = models.DateTimeField()
+    dose_era_end_date = models.DateTimeField()
+
+    class Meta:
+        db_table = "DOSE_ERA"
+
+
+class CONDITION_ERA(models.Model):
+    condition_era_id = AutoField()
+    person_id = models.ForeignKey(PERSON, on_delete=models.DO_NOTHING, related_name="condition_era")
+    condition_concept_id = models.ForeignKey(CONCEPT, on_delete=models.DO_NOTHING,
+                                             limit_choices_to=Q(domain__domain_id__icontains='Condition'), related_name="+")
+    condition_era_start_date = models.DateTimeField()
+    condition_era_end_date = models.DateTimeField()
+    condition_occurrence_count = models.IntegerField(null=True)
+
+    class Meta:
+        db_table = "CONDITION_ERA"
